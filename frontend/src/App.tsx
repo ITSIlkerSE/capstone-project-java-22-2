88abc9ff3e20a55696b95fabd84a3bc194c11326
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-11-01 18:47:13 +0100

    Add deletefunction    - editfunction partially added (not final)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,14 @@ import AdminCreationPage from "./pages/AdminCreationPage";
 
 function App() {
 
-    const {addComponent, components} = useComponent();
+    const {addComponent, components, deleteFunction} = useComponent();
 
 
     return (
         <div className="App">
             <h1>Hey Admin!</h1>
             <h3> Welcome to Check&Computify </h3>
-            <AdminCreationPage addComponent={addComponent} components={components}/>
+            <AdminCreationPage addComponent={addComponent} components={components} deleteComponent={deleteFunction}/>
         </div>
     );
 }
