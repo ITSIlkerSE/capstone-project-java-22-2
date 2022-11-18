309f06d002364b86a553d0b77334d9b2a037ee51
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-11-18 02:38:24 +0100

    Add final features    - pages    - stylings    - bugfixes

--- frontend/src/components/ResultCalc.tsx
+++ frontend/src/components/ResultCalc.tsx
@@ -14,34 +14,34 @@ function ResultCalc(props: myResultProps) {
     let display = "";
     let info = "";
 
-   if(score >= 5045){
-       display = "high-end"
-       info = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet dolor ut lacus dapibus efficitur vitae ut quam. Pellentesque mollis volutpat orci vel egestas."
-    }else if(score >= 3500 && score < 5045){
-       display = "mid-level"
-       info = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet dolor ut lacus dapibus efficitur vitae ut quam. Pellentesque mollis volutpat orci vel egestas."
-   }else{
-       display = "low-end"
-       info = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet dolor ut lacus dapibus efficitur vitae ut quam. Pellentesque mollis volutpat orci vel egestas."
-   }
-
+    if (score >= 5045) {
+        display = "high-end"
+        info = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet dolor ut lacus dapibus efficitur vitae ut quam. Pellentesque mollis volutpat orci vel egestas."
+    } else if (score >= 3500 && score < 5045) {
+        display = "mid-level"
+        info = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet dolor ut lacus dapibus efficitur vitae ut quam. Pellentesque mollis volutpat orci vel egestas."
+    } else {
+        display = "low-end"
+        info = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet dolor ut lacus dapibus efficitur vitae ut quam. Pellentesque mollis volutpat orci vel egestas."
+    }
 
 
     return (
         <>
             <h2 id={"result"}>Result</h2>
             <div className={"edit__card pop-in"}>
-                {props.result.map((item: any) => <div style={{display: "flex"}}><p style={{flex: "1"}}>{item.category}</p>
+                {props.result.map((item: any) => <div style={{display: "flex"}}><p
+                    style={{flex: "1"}}>{item.category}</p>
                     <p style={{flex: "1"}}>{item.name}</p></div>)}
 
             </div>
-<div className={"result pop-in"}>
-    <h3>Estimated price: {price} €</h3>
-    <h3>Score: {score} Points</h3>
+            <div className={"result pop-in"}>
+                <h3>Estimated price: {price} €</h3>
+                <h3>Score: {score} Points</h3>
 
-    <h3>Classification: {display}</h3>
-    <p>{info}</p>
-</div>
+                <h3>Classification: {display}</h3>
+                <p>{info}</p>
+            </div>
 
         </>
     )
